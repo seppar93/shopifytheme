65c1bc9024c0996d0202fad091b0034bad8ae936
Sepehr Parirokh <sepehr.parirokh@autodesk.com>
2021-06-03 19:24:24 -0400

    adding articles

--- assets/application.js
+++ assets/application.js
@@ -1,1 +1,11 @@
 // Put your application javascript here
+if(document.getElementById('sort_by') !== null){
+
+    document.querySelector('#sort_by').addEventListener('change', (event) => {
+        
+        let url = new URL (window.location.href)
+        url.searchParams.set('sort_by', event.currentTarget.value)
+        
+        window.location = url.href;
+    })
+}
